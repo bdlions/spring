0b4e57d027fcb71b4c6957b316807d5fae496305
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-08-27 10:21:04 +0600

    bug fixing

--- application/views/nonmember/templates/main.php
+++ application/views/nonmember/templates/main.php
@@ -35,7 +35,9 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <?php $this->load->view('nonmember/templates/sections/header'); ?>
-                        <?php echo $contents; ?>
+                        <div class="margin-top-20px">
+                            <?php echo $contents; ?>
+                        </div>
                     </div>
                 </div>
             </div>  
